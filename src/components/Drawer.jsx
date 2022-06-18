c87f0187fac928caba485ca6426577a76ad38388
sanmoyano <smoyano.di@gmail.com>
2022-06-18 12:13:55 -0300

    adding drawer

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,11 +1,102 @@
-import { Box, Button } from "@chakra-ui/react";
-import { useState } from "react";
+import { Button, FormControl, Input, Stack, Text, useToast } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { useEffect } from "react";
 
-const Drawer = ({ open }) => {
+import Contador from "./Contador";
+
+const Drawer = ({
+    arrayItems,
+    cantidad,
+    image,
+    input,
+    open,
+    setArrayItems,
+    setCantidad,
+    setImage,
+    setInput,
+}) => {
+    //CONFIGURACIONES
+    const formStack = {
+        alignItems: "center",
+        backdropFilter: "blur(10px)",
+        bgColor: "gray.900",
+        borderRadius: "lg",
+        borderWidth: "1px",
+        justifyContent: "center",
+        minWidth: "600px",
+        opacity: 0.9,
+        p: 6,
+    };
+    const toast = useToast();
+
+    //OBJETO
+    const item = {
+        id: Math.floor(Math.random() * 100),
+        name: input,
+        cantidad: cantidad,
+        image: image,
+    };
+    //FUNCIONES
+    const addItem = (e) => {
+        e.preventDefault();
+        if (input === "") {
+            toast({
+                title: "Error",
+                description: "El nombre del item no puede estar vacio",
+                status: "error",
+                duration: 2000,
+                isClosable: true,
+            });
+        } else if (arrayItems.find((item) => item.name === input)) {
+            toast({
+                title: "Advertencia",
+                description: "El item ya existe",
+                status: "warning",
+                duration: 2000,
+                isClosable: true,
+            });
+        } else {
+            setArrayItems([...arrayItems, item]);
+            setInput("");
+            setCantidad(1);
+            setImage("");
+        }
+    };
+
+    //SET LOCALSTORAGE -> hay que borrar/comentar el React.StrictMode del main.jsx para que funcione
+    useEffect(() => {
+        const datos = localStorage.getItem("arrayItems");
+
+        if (datos) {
+            setArrayItems(JSON.parse(datos));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("arrayItems", JSON.stringify(arrayItems));
+    }, [arrayItems]);
+
     return (
-        <Box bg="tomato" color="white" display={open ? "none" : "flex"} p={4} w="100%">
-            This is the Box
-        </Box>
+        <Stack {...formStack} display={open ? "flex" : "none"}>
+            <FormControl as="form" onSubmit={addItem}>
+                <Stack alignItems="center" direction={"row"}>
+                    <Input
+                        placeholder="Agregar item..."
+                        value={input}
+                        width={"auto"}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <Input
+                        placeholder="Agregar url imagen..."
+                        value={image}
+                        width={"auto"}
+                        onChange={(e) => setImage(e.target.value)}
+                    />
+                    <Contador cantidad={cantidad} setCantidad={setCantidad} />
+                    <Button onClick={addItem}>Add</Button>
+                </Stack>
+            </FormControl>
+        </Stack>
     );
 };
 
--- src/components/ListaDeRegalos.jsx
+++ src/components/ListaDeRegalos.jsx
@@ -1,110 +1,37 @@
 import { Button, FormControl, Input, Stack, useToast } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import RegalosList from "./RegalosList";
-import Contador from "./Contador";
 import Drawer from "./Drawer";
 
 const ListaDeRegalos = () => {
-    //CONFIGURACIONES
-    const formStack = {
-        alignItems: "center",
-        backdropFilter: "blur(10px)",
-        bgColor: "gray.900",
-        borderRadius: "lg",
-        borderWidth: "1px",
-        justifyContent: "center",
-        minWidth: "600px",
-        opacity: 0.9,
-        p: 6,
-    };
-    const toast = useToast();
-
+    const [isOpen, setIsOpen] = useState(false);
     //ESTADOS
     const [arrayItems, setArrayItems] = useState([]);
     const [input, setInput] = useState("");
     const [cantidad, setCantidad] = useState(1);
     const [image, setImage] = useState("");
-    const [isOpen, setIsOpen] = useState(false);
-
-    //OBJETO
-    const item = {
-        id: Math.floor(Math.random() * 100),
-        name: input,
-        cantidad: cantidad,
-        image: image,
-    };
-    //FUNCIONES
-    const addItem = (e) => {
-        e.preventDefault();
-        if (input === "") {
-            toast({
-                title: "Error",
-                description: "El nombre del item no puede estar vacio",
-                status: "error",
-                duration: 2000,
-                isClosable: true,
-            });
-        } else if (arrayItems.find((item) => item.name === input)) {
-            toast({
-                title: "Advertencia",
-                description: "El item ya existe",
-                status: "warning",
-                duration: 2000,
-                isClosable: true,
-            });
-        } else {
-            setArrayItems([...arrayItems, item]);
-            setInput("");
-            setCantidad(1);
-            setImage("");
-        }
-    };
 
     const openDrawer = () => {
         setIsOpen(!isOpen);
     };
 
-    //SET LOCALSTORAGE -> hay que borrar/comentar el React.StrictMode del main.jsx para que funcione
-    useEffect(() => {
-        const datos = localStorage.getItem("arrayItems");
-
-        if (datos) {
-            setArrayItems(JSON.parse(datos));
-        }
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem("arrayItems", JSON.stringify(arrayItems));
-    }, [arrayItems]);
-
     return (
-        <>
+        <Stack>
             <Button onClick={openDrawer}>Agregar items</Button>
-            <Drawer open={isOpen} />
-
-            <Stack {...formStack}>
-                <FormControl as="form" onSubmit={addItem}>
-                    <Stack alignItems="center" direction={"row"}>
-                        <Input
-                            placeholder="Agregar item..."
-                            value={input}
-                            width={"auto"}
-                            onChange={(e) => setInput(e.target.value)}
-                        />
-                        <Input
-                            placeholder="Agregar url imagen..."
-                            value={image}
-                            width={"auto"}
-                            onChange={(e) => setImage(e.target.value)}
-                        />
-                        <Contador cantidad={cantidad} setCantidad={setCantidad} />
-                        <Button onClick={addItem}>Add</Button>
-                    </Stack>
-                </FormControl>
-            </Stack>
+            <Drawer
+                arrayItems={arrayItems}
+                cantidad={cantidad}
+                image={image}
+                input={input}
+                open={isOpen}
+                setArrayItems={setArrayItems}
+                setCantidad={setCantidad}
+                setImage={setImage}
+                setInput={setInput}
+            />
             <RegalosList arrayItems={arrayItems} setArrayItems={setArrayItems} />
-        </>
+        </Stack>
     );
 };
 
